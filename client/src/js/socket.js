e35bffd6eaeec444fe0b0611d1b6e823d6817946
Neil Tate <57351607+Neil-Tate@users.noreply.github.com>
2020-05-05 17:38:27 +0200

    update 888

--- client/src/js/socket.js
+++ client/src/js/socket.js
@@ -1,5 +1,6 @@
 import io from 'socket.io-client';
 
-const socket = io({ path: '/bridge' });
+// const socket = io({ path: '/bridge' });
+const socket = io.connect(`http://176.9.208.138:5000/`);
 
 export default socket;
--- server/lib/socket.js
+++ server/lib/socket.js
@@ -6,40 +6,40 @@ const users = require('./users');
  * @param {SocketIO.Socket} socket
  */
 function initSocket(socket) {
-  let id;
-  socket
-    .on('init', async () => {
-      id = await users.create(socket);
-      socket.emit('init', { id });
-    })
-    .on('request', (data) => {
-      const receiver = users.get(data.to);
-      if (receiver) {
-        receiver.emit('request', { from: id });
-      }
-    })
-    .on('call', (data) => {
-      const receiver = users.get(data.to);
-      if (receiver) {
-        receiver.emit('call', { ...data, from: id });
-      } else {
-        socket.emit('failed');
-      }
-    })
-    .on('end', (data) => {
-      const receiver = users.get(data.to);
-      if (receiver) {
-        receiver.emit('end');
-      }
-    })
-    .on('disconnect', () => {
-      users.remove(id);
-      console.log(id, 'disconnected');
-    });
+    let id;
+    socket
+        .on('init', async() => {
+            id = await users.create(socket);
+            socket.emit('init', { id });
+        })
+        .on('request', (data) => {
+            const receiver = users.get(data.to);
+            if (receiver) {
+                receiver.emit('request', { from: id });
+            }
+        })
+        .on('call', (data) => {
+            const receiver = users.get(data.to);
+            if (receiver) {
+                receiver.emit('call', {...data, from: id });
+            } else {
+                socket.emit('failed');
+            }
+        })
+        .on('end', (data) => {
+            const receiver = users.get(data.to);
+            if (receiver) {
+                receiver.emit('end');
+            }
+        })
+        .on('disconnect', () => {
+            users.remove(id);
+            console.log(id, 'disconnected');
+        });
 }
 
 module.exports = (server) => {
-  io({ path: '/bridge', serveClient: false })
-    .listen(server, { log: true })
-    .on('connection', initSocket);
+    io({ serveClient: false })
+        .listen(server, { log: true })
+        .on('connection', initSocket);
 };
